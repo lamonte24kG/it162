090d91a0132133d100bd17dfbf4d457ab3776a6d
lamonte24kG <141269004+lamonte24kG@users.noreply.github.com>
2023-12-15 23:34:18 -0800

    Add files via upload

---
+++ fp/index.php
@@ -0,0 +1,11 @@
+<?php include 'includes/header.php';?>
+    
+        <main class="main">
+            <h1>Danny Godinez</h1>
+            <h3>Professional Music Instructor Seattle Washington</h3>
+          <p>Where music education meets passion and creativity.</p>
+        </main>
+
+    </div>
+   <!--end wrapper-->
+   <?php include 'includes/footer.php';?>
